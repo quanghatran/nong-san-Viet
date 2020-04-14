c4dc27f3f920ec6b87ab5d58ce3179a5b7667774
quanghatran <quanghatran2161998@gmail.com>
2020-04-14 10:25:31 +0700

    04/14/2020

--- resources/js/main.js
+++ resources/js/main.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+//home button
+//home button 
+var myButton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFuntion()};
+
+function scrollFuntion() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display ="none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
